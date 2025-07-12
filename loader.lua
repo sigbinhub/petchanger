1a0363671c8393f4567bdd4f4aa58f7a95221e32
sigbinhub <joshuahindepogi@gmail.com>
2025-07-12 16:08:46 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,8 +1,2 @@
--- Load the first script (SigbinHub Pet Changer)
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigbinhub/pet-changer-beta/refs/heads/main/loader.lua"))()
-
--- Delay to ensure first script is fully initialized
-task.delay(3, function()
-    -- Load the second script after a short delay
-    loadstring(game:HttpGet("https://cdn.sourceb.in/bins/RVzbujTndB/0", true))()
-end)
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/RVzbujTndB/0", true))()
