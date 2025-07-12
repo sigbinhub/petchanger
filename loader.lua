6a2776a7f3c2db26ec3ef83ed35848d0289b5948
sigbinhub <joshuahindepogi@gmail.com>
2025-07-12 16:13:19 +0800

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,2 +1,2 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/sigbinhub/pet-changer-beta/refs/heads/main/loader.lua"))()
-loadstring(game:HttpGet("https://cdn.sourceb.in/bins/RVzbujTndB/0", true))()
+.in/bins/RVzbujTndB/0", true))()
